f0845603a9a854578f64853720c83d7a8de799d5
rya <ruilede@qq.com>
2024-09-07 04:01:34 +0000

    Update 爱看hd.js

--- js/爱看hd.js
+++ js/爱看hd.js
@@ -5,7 +5,7 @@ var rule={
     title:'爱看',
     模板:'mxpro',
     // host:'https://akanhd.com',
-    host:'https://aikanys.vip',
+    host:'https://www.aikanys.pro',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,"li:eq(2)&&a&&href");print(src);HOST=src',
     url:'/vodshow/fyfilter/',
     searchUrl: '/rss.xml?wd=**',
